a230e0261bf5b73b65bfe645333bc2ee787c029c
Adam Ballinger <crunchienut20@gmail.com>
2017-07-18 23:02:32 +0100

    Initial commit.
    Project creation.

---
+++ Assets/Packages/ScriptNamespaceProcessor/ScriptNamespaceProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+// ReSharper disable CheckNamespace
+
+namespace ScriptNamespaceProcessor
+{
+	public class ScriptNamespaceProcessor : UnityEditor.AssetModificationProcessor 
+	{
+
+        private static void OnWillCreateAsset(string _path)
+        {
+            // Remove the .meta from path since this function only applies to meta files, however the normal .cs file will still be created along side.
+            _path = _path.Replace(".meta", "");
+            var index = _path.IndexOf(".", StringComparison.Ordinal);
+
+            if (index < 0) return;
+
+            var file = _path.Substring(index);
+
+            // only want to modify files that are .cs
+            if (file != ".cs") return;
+
+            // Create the full file path relative to the system directory, rather than the local unity path (E.g. Assets/Scripts to C:/blah blah.
+            index = Application.dataPath.LastIndexOf("Assets", StringComparison.Ordinal);
+            _path = Application.dataPath.Substring(0, index) + _path;
+
+            // Ensure the file actually exists..
+            if (!File.Exists(_path)) return;
+
+            // Store the original contents of the file, and replace the #NAMESPACE# phrase in the generated script template with the actual file namespace 
+            var fileContent = File.ReadAllText(_path);
+            var fileNamespace = GetNameSpaceFromPath(_path);
+            fileContent = fileContent.Replace("#NAMESPACE#", fileNamespace);
+
+            // Write the file contents with the added namespace back to the file.
+            File.WriteAllText(_path, fileContent);
+
+            // Notifiy unity it needs to reimport the modified file.
+            AssetDatabase.Refresh();
+        }
+
+        /// <summary>
+        /// Returns a script namespace from its given file path.
+        /// </summary>
+        /// <param name="_path">System directory to file.</param>
+        /// <returns></returns>
+        private static string GetNameSpaceFromPath(string _path)
+        {
+            // Trim the path down to only the Assets/... directory
+            var assetsRootIndex = _path.IndexOf("Assets", StringComparison.Ordinal);
+            var assetsRoot = _path.Substring(assetsRootIndex);
+
+            // Remove the file name from the namespace
+            var lastSlashIndex = assetsRoot.LastIndexOf("/", StringComparison.Ordinal);
+            assetsRoot = assetsRoot.Remove(lastSlashIndex);
+
+            // Replace all "/" in directory with "." for namespace
+            var fileNamespace = assetsRoot.Replace("/", ".");
+
+            return fileNamespace;
+        }
+	}
+}
