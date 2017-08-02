3ad7c5cf6e99908146f101f5e632897001bd2dd5
Adam Ballinger <crunchienut20@gmail.com>
2017-08-02 15:17:43 +0100

    Camera now starts in center of world and is constrained.

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -1,26 +1,37 @@
+using Models.World;
 using UnityEngine;
 
 namespace Controllers
 {
-	public class CameraController : MonoBehaviour
-	{
+    public class CameraController : MonoBehaviour
+    {
 
-	    public float maxZoom = 5.0f;
-	    public float minZoom = 15.0f;
-	    public float zoomSpeed = 1.5f;
+        public float maxZoom = 5.0f;
+        public float minZoom = 15.0f;
+        public float zoomSpeed = 1.5f;
 
-	    public float cameraMoveSpeed = 1.0f;
+        public float cameraMoveSpeed = 1.0f;
 
-	    private new Camera camera;
-	    private Transform cameraTransform;
+        private new Camera camera;
+        private Transform cameraTransform;
 
-	    private Vector2 currentMousePosition;
-	    private Vector2 previousMousePosition;
+        private Vector2 currentMousePosition;
+        private Vector2 previousMousePosition;
+
+        private Vector3 cameraPosition;
+
+        private int minCameraX = 0, maxCameraX;
+        private int minCameraY = 0, maxCameraY;
 
         private void Start()
         {
             camera = Camera.main;
             cameraTransform = camera.GetComponent<Transform>();
+
+            maxCameraX = World.Instance.Width;
+            maxCameraY = World.Instance.Height;
+
+            cameraTransform.position = new Vector3(maxCameraX  / 2, maxCameraY / 2, cameraTransform.position.z);
         }
 
         private void Update()
@@ -31,11 +42,16 @@ namespace Controllers
             HandleCameraZoom();
 
             previousMousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+
+            cameraPosition = cameraTransform.position;
+            cameraPosition.x = Mathf.Clamp(cameraPosition.x, minCameraX, maxCameraX);
+            cameraPosition.y = Mathf.Clamp(cameraPosition.y, minCameraY, maxCameraY);
+            cameraTransform.position = cameraPosition;
         }
 
         private void HandleCameraMovement()
         {
-            if(Input.GetMouseButton(1) || Input.GetMouseButton(2))
+            if (Input.GetMouseButton(1) || Input.GetMouseButton(2))
             {
                 cameraTransform.Translate(previousMousePosition - currentMousePosition);
             }
@@ -49,5 +65,5 @@ namespace Controllers
             camera.orthographicSize -= camera.orthographicSize * Input.GetAxis("Mouse ScrollWheel") * (zoomSpeed * Time.deltaTime);
             camera.orthographicSize = Mathf.Clamp(camera.orthographicSize, maxZoom, minZoom);
         }
-	}
+    }
 }
