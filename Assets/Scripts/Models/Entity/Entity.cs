9355960128f5b438479671d3299b2d851c3ac94c
Adam Ballinger <crunchienut20@gmail.com>
2017-08-08 13:46:39 +0100

    Added SetPosition and Name to Entity.

--- Assets/Scripts/Models/Entity/Entity.cs
+++ Assets/Scripts/Models/Entity/Entity.cs
@@ -1,4 +1,6 @@
+using System;
 using Models.Map;
+using UnityEngine;
 
 namespace Models.Entity
 {
@@ -7,12 +9,17 @@ namespace Models.Entity
         /// <summary>
         /// Precise X coordinate of entity.
         /// </summary>
-        public float X { get; }
+        public float X { get; protected set; }
 
         /// <summary>
         /// Precise Y coordinate of entity.
         /// </summary>
-        public float Y { get; }
+        public float Y { get; protected set; }
+
+        /// <summary>
+        /// Entity's name.
+        /// </summary>
+        public string Name { get; set; }
 
 	    /// <summary>
 	    /// The current tile the pivot of the entity is placed within.
@@ -24,11 +31,24 @@ namespace Models.Entity
         /// </summary>
         public EntitySpriteData SpriteData { get; set; }
 
-
         protected Entity(int _x, int _y)
         {
             X = _x;
             Y = _y;
+        }
+
+        /// <summary>
+        /// Set the position of the entity in the world. Position is clamped within the bounds of the world.
+        /// </summary>
+        /// <param name="_x"></param>
+        /// <param name="_y"></param>
+        public void SetPosition(float _x, float _y)
+        {
+            X = _x;
+            Y = _y;
+
+            X = Mathf.Clamp(X, 0, World.Instance.Width - 1);
+            Y = Mathf.Clamp(Y, 0, World.Instance.Height - 1);
         }
 	}
 }
