39e00861814d7e7d6f14d705efa7ed3a639d70ca
Adam Ballinger <crunchienut20@gmail.com>
2018-06-01 22:22:17 +0100

    Update SpriteCache to new SpriteData system

--- Assets/Scripts/Controllers/SpriteCache.cs
+++ Assets/Scripts/Controllers/SpriteCache.cs
@@ -10,13 +10,36 @@ namespace Controllers
 
         public static Sprite GetSprite(string _spriteName)
         {
-            //return spriteCache.ContainsKey(_spriteName) ? spriteCache[_spriteName] : null;
-            return null;
+            return GetSprite("default", _spriteName);
         }
 
         public static Sprite GetSprite(string _spriteGroup, string _spriteName)
         {
-            return GetSprite(_spriteName);
+            if(spriteCache.ContainsKey(_spriteGroup))
+            {
+                foreach (var sprite in spriteCache[_spriteGroup])
+                {
+                    if (sprite.name.Equals(_spriteName))
+                    {
+                        return sprite;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static Sprite GetSprite(string _spriteGroup, int _groupIndex)
+        {
+            if(spriteCache.ContainsKey(_spriteGroup))
+            {
+                if(spriteCache[_spriteGroup].Count <= _groupIndex)
+                {
+                    return spriteCache[_spriteGroup][_groupIndex];
+                }
+            }
+
+            return null;
         }
 
         public static Sprite GetSprite(SpriteData _data)
@@ -24,15 +47,26 @@ namespace Controllers
             return null;
         }
 
-        public static void AddSprite(string _spriteGroup, string _spriteName, Sprite _sprite)
+        public static void AddSprite(string _spriteGroup, Sprite _sprite)
         {
-            if(spriteCache.ContainsKey(_spriteName))
+            if(_sprite == null)
             {
-                // Already exists in cache.
+                // Don't add null sprites to the cache
                 return;
             }
 
-            //spriteCache.Add(_spriteName, _sprite);
+            if(!spriteCache.ContainsKey(_spriteGroup))
+            {
+                spriteCache.Add(_spriteGroup, new List<Sprite>());
+            }
+
+            if(spriteCache[_spriteGroup].Contains(_sprite))
+            {
+                // Sprite already exists in the cache for this group
+                return;
+            }
+
+            spriteCache[_spriteGroup].Add(_sprite);
         }
     }
 }
