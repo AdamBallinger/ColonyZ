e2bea612b160c05437470a9df40ff4f62a4b7587
Adam Ballinger <crunchienut20@gmail.com>
2019-04-01 16:15:10 +0100

    Fix MeshRenderer sorting layer not saving on change

--- Assets/Scripts/Editor/MeshRendererSortingEditor.cs
+++ Assets/Scripts/Editor/MeshRendererSortingEditor.cs
@@ -1,47 +1,97 @@
-﻿using System.Linq;
-using UnityEditor;
+﻿ 
 using UnityEngine;
-
+using UnityEditor;
+using UnityEditorInternal;
+using System.Reflection;
+using System;
+ 
+//Expose SortingLayer  SortingOrder on MeshRenderer
+//With nice drop down and revert to prefab functionality.
+ 
+//Base exposing code by neror http://forum.unity3d.com/threads/212006-Drawing-order-of-Meshes-and-Sprites
+//Get all sorting layer name and ID by guavaman  Ivan.Murashko http://answers.unity3d.com/questions/585108/how-do-you-access-sorting-layers-via-scripting.html
+//Sorting Layer drop down menu, bold text on prefab override, revert to prefab and instant update on Order change functionality by 5argon
+ 
 [CustomEditor(typeof(MeshRenderer))]
-public class MeshRendererSortingEditor : Editor
+ 
+public class MeshRendererSortingLayersEditor : Editor
 {
+ 
     public override void OnInspectorGUI()
     {
+ 
         base.OnInspectorGUI();
-
+ 
+        serializedObject.Update();
+ 
+        SerializedProperty sortingLayerID = serializedObject.FindProperty("m_SortingLayerID");
+        SerializedProperty sortingOrder = serializedObject.FindProperty("m_SortingOrder");
+ 
         MeshRenderer renderer = target as MeshRenderer;
-
-
-        var layers = SortingLayer.layers;
-
-        EditorGUILayout.BeginHorizontal();
+ 
+        Rect firstHoriz = EditorGUILayout.BeginHorizontal();
+ 
         EditorGUI.BeginChangeCheck();
-        int newId = DrawSortingLayersPopup(renderer.sortingLayerID);
-        if(EditorGUI.EndChangeCheck()) {
-            renderer.sortingLayerID = newId;
-        }
-        EditorGUILayout.EndHorizontal();
-
-        EditorGUILayout.BeginHorizontal();
-        EditorGUI.BeginChangeCheck();
-        int order = EditorGUILayout.IntField("Sorting Order", renderer.sortingOrder);
-        if(EditorGUI.EndChangeCheck()) {
-            renderer.sortingOrder = order;
-        }
-        EditorGUILayout.EndHorizontal();
-
-    }
-
-    int DrawSortingLayersPopup(int layerID) {
-        var layers = SortingLayer.layers;
-        var names = layers.Select(l => l.name).ToArray();
-        if (!SortingLayer.IsValid(layerID))
+ 
+        EditorGUI.BeginProperty(firstHoriz,GUIContent.none,sortingLayerID);
+ 
+        string[] layerNames = GetSortingLayerNames();
+        int[] layerID = GetSortingLayerUniqueIDs();
+ 
+        int selected = -1;
+        //What is selected?
+        int sID = sortingLayerID.intValue;
+        for(int i = 0 ; i < layerID.Length ; i++)
         {
-            layerID = layers[0].id;
+            //Debug.Log(sID + " " + layerID[i]);
+            if(sID == layerID[i])
+            {
+                selected = i;
+            }
         }
-        var layerValue = SortingLayer.GetLayerValueFromID(layerID);
-        var newLayerValue = EditorGUILayout.Popup("Sorting Layer", layerValue, names);
-        return layers[newLayerValue].id;
+ 
+        if(selected == -1)
+        {
+            //Select Default.
+            for(int i = 0 ; i < layerID.Length ; i++)
+            {
+                if(layerID[i] == 0)
+                {
+                    selected = i;
+                }
+            }
+        }
+ 
+        selected = EditorGUILayout.Popup("Sorting Layer" ,selected,layerNames);
+ 
+        //Translate to ID
+        sortingLayerID.intValue = layerID[selected];
+ 
+ 
+        EditorGUI.EndProperty();
+ 
+        EditorGUILayout.EndHorizontal();
+ 
+        EditorGUILayout.BeginHorizontal();
+        EditorGUI.BeginChangeCheck();
+ 
+        EditorGUILayout.PropertyField(sortingOrder,new GUIContent("Order in Layer"));
+ 
+ 
+        EditorGUILayout.EndHorizontal();
+        serializedObject.ApplyModifiedProperties();
     }
-
+ 
+    public string[] GetSortingLayerNames() {
+        Type internalEditorUtilityType = typeof(InternalEditorUtility);
+        PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+        return (string[])sortingLayersProperty.GetValue(null, new object[0]);
+    }
+ 
+    public int[] GetSortingLayerUniqueIDs() {
+        Type internalEditorUtilityType = typeof(InternalEditorUtility);
+        PropertyInfo sortingLayerUniqueIDsProperty = internalEditorUtilityType.GetProperty("sortingLayerUniqueIDs", BindingFlags.Static | BindingFlags.NonPublic);
+        return (int[])sortingLayerUniqueIDsProperty.GetValue(null, new object[0]);
+    }
+ 
 }
