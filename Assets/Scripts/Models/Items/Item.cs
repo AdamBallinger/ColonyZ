14ea1d2d0fdbf28deb60aeb9205f470575c51594
Adam Ballinger <crunchienut20@gmail.com>
2019-10-08 13:36:16 +0100

    Add basic Item classes and asset file.

---
+++ Assets/Scripts/Models/Items/Item.cs
@@ -0,0 +1,29 @@
+using Models.Sprites;
+using UnityEngine;
+
+namespace Models.Items
+{
+    public abstract class Item : ScriptableObject
+    {
+        public SpriteData ItemSpriteData => itemSpriteData;
+
+        public string ItemName => itemName;
+
+        public int MaxDurability => maxDurability;
+        
+        public int Durability { get; protected set; }
+
+        #region Serialized Fields
+
+        [SerializeField]
+        private SpriteData itemSpriteData;
+        
+        [SerializeField]
+        private string itemName;
+
+        [SerializeField]
+        private int maxDurability;
+
+        #endregion
+    }
+}
