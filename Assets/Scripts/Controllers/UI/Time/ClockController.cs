4e6794557c5a55cc6a586dfe98cea8fcb485f738
Crunchy Nut <crunchienut20@gmail.com>
2020-03-26 14:38:10 +0000

    Add current day and temperature (NYI) text under clock controls.

--- Assets/Scripts/Controllers/UI/Time/ClockController.cs
+++ Assets/Scripts/Controllers/UI/Time/ClockController.cs
@@ -6,25 +6,29 @@ namespace Controllers.UI.Time
 {
     public class ClockController : MonoBehaviour
     {
-        [SerializeField]
-        private TMP_Text clockText;
+        [SerializeField] private TMP_Text clockText;
 
-        [SerializeField]
-        private TMP_Text modeText;
-        
+        [SerializeField] private TMP_Text modeText;
+
+        [SerializeField] private TMP_Text dayText;
+
+        [SerializeField] private TMP_Text temperatureText;
+
         private void Start()
         {
             TimeManager.Instance.timeModeChangedEvent += OnTimeModeChanged;
             TimeManager.Instance.timeChangedEvent += OnTimeChanged;
+            TimeManager.Instance.newDayEvent += day => dayText.text = "Day: " + day;
 
             modeText.text = $"{(int) TimeManager.Instance.TimeMode}x";
+            dayText.text = "Day: " + TimeManager.Instance.Day;
         }
-        
+
         private void OnTimeModeChanged(TimeMode _newMode)
         {
-            modeText.text = $"{(int)_newMode}x";
+            modeText.text = $"{(int) _newMode}x";
         }
-        
+
         private void OnTimeChanged(int _hour, int _minute)
         {
             clockText.text = $"{_hour:00}:{_minute:00}";
