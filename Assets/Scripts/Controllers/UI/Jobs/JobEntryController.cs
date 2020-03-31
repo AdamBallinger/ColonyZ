e4f7491d10e61c3b663a762c10c3688f396b013e
Crunchy Nut <crunchienut20@gmail.com>
2020-03-31 13:30:15 +0100

    Remove redundant job state event listener in JobEntryController.

--- Assets/Scripts/Controllers/UI/Jobs/JobEntryController.cs
+++ Assets/Scripts/Controllers/UI/Jobs/JobEntryController.cs
@@ -8,22 +8,11 @@ namespace Controllers.UI.Jobs
     {
         [SerializeField] private TMP_Text jobNameText;
 
-        private Job job;
-
         public void Set(Job _job)
         {
-            job = _job;
-            JobManager.Instance.jobStateChangedEvent += StateChanged;
             jobNameText.text = _job.JobName;
-        }
-
-        private void StateChanged(Job _job)
-        {
-            if (_job == job)
-            {
-                jobNameText.color = job.State == JobState.Active ? Color.green :
-                    job.State == JobState.Error ? Color.red : Color.gray;
-            }
+            jobNameText.color = _job.State == JobState.Active ? Color.green :
+                _job.State == JobState.Error ? Color.red : Color.gray;
         }
     }
 }
