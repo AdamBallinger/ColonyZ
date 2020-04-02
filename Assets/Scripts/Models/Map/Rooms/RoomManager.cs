cd8bad01436c920de211c50652fbefee34b9ea5c
Crunchy Nut <crunchienut20@gmail.com>
2020-04-02 14:43:13 +0100

    Remove unnecessary null check in RoomManager::RemoveRoom.

--- Assets/Scripts/Models/Map/Rooms/RoomManager.cs
+++ Assets/Scripts/Models/Map/Rooms/RoomManager.cs
@@ -10,11 +10,11 @@ namespace Models.Map.Rooms
         public static RoomManager Instance { get; private set; }
 
         public static int OUTSIDE_ROOM_ID => 0;
-        
+
         public Room OutsideRoom { get; }
-        
+
         private List<Room> Rooms { get; }
-        
+
         private RoomManager()
         {
             OutsideRoom = new Room();
@@ -22,34 +22,34 @@ namespace Models.Map.Rooms
             {
                 OutsideRoom
             };
-            
+
             OutsideRoom.AddConnection(OutsideRoom);
         }
-        
+
         public static void Create()
         {
             if (Instance != null) return;
-            
+
             Instance = new RoomManager();
         }
-        
+
         public int GetRoomID(Room _room)
         {
             return Rooms.IndexOf(_room);
         }
-        
-        public void RemoveRoom(Room _room)
-        {
-            if (_room == null || _room != null && _room.RoomID == OUTSIDE_ROOM_ID) return;
 
-            _room?.ReleaseTilesToOutside();
+        private void RemoveRoom(Room _room)
+        {
+            if (_room == null || _room.RoomID == OUTSIDE_ROOM_ID) return;
+
+            _room.ReleaseTilesToOutside();
             Rooms.Remove(_room);
         }
 
         public void CheckForRoom(Tile _tile)
         {
             var oldRoom = _tile.Room;
-            
+
             // An enclosing object was built on this tile.
             if (oldRoom != null)
             {
@@ -61,7 +61,7 @@ namespace Models.Map.Rooms
 
                 // Remove the source tile from its current room, as enclosing tiles do not belong to any rooms.
                 oldRoom.UnassignTile(_tile);
-                
+
                 // Delete the old source tile room as it is no longer needed (Unless it was the outside room).
                 RemoveRoom(oldRoom);
             }
@@ -74,14 +74,14 @@ namespace Models.Map.Rooms
                 {
                     RemoveRoom(tile.Room);
                 }
-                
+
                 // Once the rooms are removed, flood from the source tile to create the new room.
                 FloodFill(_tile, null);
             }
-            
+
             GenerateRoomConnections();
         }
-        
+
         /// <summary>
         /// Perform a flood fill on a given tile to check if a new room needs to be created.
         /// </summary>
@@ -99,7 +99,7 @@ namespace Models.Map.Rooms
             if (_startTile.Room != _oldRoom) return;
 
             var newRoom = new Room();
-            
+
             var tilesToCheck = new Queue<Tile>();
             tilesToCheck.Enqueue(_startTile);
 
@@ -112,15 +112,15 @@ namespace Models.Map.Rooms
                 if (currentTile.Room != newRoom)
                 {
                     newRoom.AssignTile(currentTile);
-                    
+
                     foreach (var neighbour in currentTile.DirectNeighbours)
                     {
                         // This neighbour has already been added to the new room.
                         if (neighbour.Room == newRoom) continue;
-                        
+
                         // Enclosing objects don't get assigned to rooms.
                         if (neighbour.HasObject && neighbour.Object.EnclosesRoom) continue;
-                        
+
                         if (neighbour.IsExposedToOutside())
                         {
                             // Room isn't valid as it is not enclosed.
@@ -129,7 +129,7 @@ namespace Models.Map.Rooms
 
                         tilesToCheck.Enqueue(neighbour);
                     }
-                    
+
                     if (connectedToOutside)
                     {
                         newRoom.ReleaseTilesToOutside();
@@ -137,14 +137,14 @@ namespace Models.Map.Rooms
                     }
                 }
             }
-            
+
             Rooms.Add(newRoom);
         }
-        
+
         private void GenerateRoomConnections()
         {
             var doors = World.Instance.Objects.OfType<DoorObject>().ToList();
-            
+
             // Clear existing connections, then re-add self connections.
             foreach (var room in Rooms)
             {
@@ -157,7 +157,7 @@ namespace Models.Map.Rooms
                 var tile = door.Tile;
                 var n = World.Instance.GetTileAt(tile.X, tile.Y + 1);
                 var s = World.Instance.GetTileAt(tile.X, tile.Y - 1);
-                
+
                 // if the tiles to the north and south of the door have different room ids, then they are connected.
                 if (n != null && s != null && n.Room?.RoomID != s.Room?.RoomID)
                 {
@@ -168,32 +168,32 @@ namespace Models.Map.Rooms
 
                 var e = World.Instance.GetTileAt(tile.X + 1, tile.Y);
                 var w = World.Instance.GetTileAt(tile.X - 1, tile.Y);
-                
+
                 if (e != null && w != null && e.Room?.RoomID != w.Room?.RoomID)
                 {
                     e.Room?.AddConnection(w.Room);
                     w.Room?.AddConnection(e.Room);
                 }
             }
-            
+
             foreach (var room in Rooms)
             {
                 var checkedRooms = new List<Room>();
                 var rooms = LinkRoom(room, checkedRooms);
-                
+
                 foreach (var r in rooms)
                 {
                     room.AddConnection(r);
                 }
             }
         }
-        
+
         private List<Room> LinkRoom(Room _room, List<Room> _checkedRooms)
         {
             foreach (var room in _room.ConnectedRooms)
             {
                 if (_checkedRooms.Contains(room)) continue;
-                
+
                 _checkedRooms.Add(room);
                 LinkRoom(room, _checkedRooms);
             }
