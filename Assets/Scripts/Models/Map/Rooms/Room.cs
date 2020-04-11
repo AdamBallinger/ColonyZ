783b9f4d1cc489fb6341beabfd6f5f731546c0fa
Crunchy Nut <crunchienut20@gmail.com>
2020-04-11 14:11:12 +0100

    Major optimisation to rooms Assign and Unassign functions.

--- Assets/Scripts/Models/Map/Rooms/Room.cs
+++ Assets/Scripts/Models/Map/Rooms/Room.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Models.Map.Tiles;
 
 namespace Models.Map.Rooms
@@ -9,20 +10,20 @@ namespace Models.Map.Rooms
         /// Unique ID for this room.
         /// </summary>
         public int RoomID => RoomManager.Instance.GetRoomID(this);
-        
+
         /// <summary>
         /// List of tiles that assigned to this room. This does not include the tiles that enclose the room.
         /// </summary>
-        public List<Tile> Tiles { get; }
-        
+        public HashSet<Tile> Tiles { get; }
+
         public List<Room> ConnectedRooms { get; }
 
         public Room()
         {
-            Tiles = new List<Tile>();
+            Tiles = new HashSet<Tile>();
             ConnectedRooms = new List<Room>();
         }
-        
+
         public void AssignTile(Tile _tile)
         {
             if (Tiles.Contains(_tile)) return;
@@ -33,7 +34,7 @@ namespace Models.Map.Rooms
             Tiles.Add(_tile);
             _tile.Room = this;
         }
-        
+
         /// <summary>
         /// Removes the tile from the room, and gives the tile a null room.
         /// </summary>
@@ -51,19 +52,20 @@ namespace Models.Map.Rooms
         /// </summary>
         public void ReleaseTilesToOutside()
         {
+            var tiles = Tiles.ToList();
             for (var i = Tiles.Count - 1; i >= 0; i--)
             {
-                RoomManager.Instance.OutsideRoom.AssignTile(Tiles[i]);
+                RoomManager.Instance.OutsideRoom.AssignTile(tiles[i]);
             }
         }
-        
+
         public void AddConnection(Room _room)
         {
             if (_room == null || ConnectedRooms.Contains(_room)) return;
-            
+
             ConnectedRooms.Add(_room);
         }
-        
+
         public bool HasConnection(Room _room)
         {
             return ConnectedRooms.Contains(_room);
