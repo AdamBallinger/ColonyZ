cf8f01637d8e142f74a03a19cb2fff70f38d7bba
Crunchy Nut <crunchienut20@gmail.com>
2020-05-19 21:24:30 +0100

    Add main menus "new game" button functionality. Load Game still WIP.

---
+++ Assets/Scripts/ColonyZ/Controllers/UI/MainMenu/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using ColonyZ.Models.Saving;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ColonyZ.Controllers.UI.MainMenu
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private Button loadGameButton;
+
+        private void Start()
+        {
+            loadGameButton.interactable = SaveGameHandler.SaveGamePresent();
+        }
+
+        public void OnNewGamePressed()
+        {
+            SceneManager.LoadScene("world");
+        }
+
+        public void OnLoadGamePressed()
+        {
+        }
+    }
+}
