1241960758385a2de7da9e2876174374b1412c1d
Adam Ballinger <crunchienut20@gmail.com>
2021-08-09 20:04:03 +0100

    Fix typo in TileObjectData.

--- Assets/Scripts/ColonyZ/Models/Map/Tiles/Objects/Data/TileObjectData.cs
+++ Assets/Scripts/ColonyZ/Models/Map/Tiles/Objects/Data/TileObjectData.cs
@@ -14,7 +14,7 @@ namespace ColonyZ.Models.Map.Tiles.Objects.Data
         [SerializeField] private bool smartObject;
         [SerializeField] private bool buildable;
         [SerializeField] private bool rotatable;
-        [SerializeField] private bool enlosesRoom;
+        [SerializeField] private bool enclosesRoom;
 
         [SerializeField] private int objectWidth;
         [SerializeField] private int objectHeight;
@@ -29,7 +29,7 @@ namespace ColonyZ.Models.Map.Tiles.Objects.Data
         public bool SmartObject => smartObject;
         public bool Buildable => buildable;
         public bool Rotatable => rotatable;
-        public bool EnclosesRoom => enlosesRoom;
+        public bool EnclosesRoom => enclosesRoom;
 
         public int ObjectWidth => objectWidth;
         public int ObjectHeight => objectHeight;
