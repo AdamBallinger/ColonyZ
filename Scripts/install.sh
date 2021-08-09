2e4c19e3ce6de0a2dc119b31c961c45fa951ed7f
Adam Ballinger <crunchienut20@gmail.com>
2021-08-09 13:42:11 +0100

    Remove travis files.

--- Scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-echo 'Downloading from http://beta.unity3d.com/download/f2970305fe1c/MacEditorInstaller/Unity-2019.1.6f1.pkg'
-curl -o Unity.pkg http://beta.unity3d.com/download/f2970305fe1c/MacEditorInstaller/Unity-2019.1.6f1.pkg
-
-echo 'Installing Unity.'
-sudo installer -dumplog -package Unity.pkg -target /
