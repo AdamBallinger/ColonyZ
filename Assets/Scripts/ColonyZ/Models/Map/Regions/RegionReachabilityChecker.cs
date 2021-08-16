1a2d08e0bc72d8ce4c72cda3128773219e5351b0
Adam Ballinger <crunchienut20@gmail.com>
2021-08-16 12:30:05 +0100

    RegionReachabilityChecker now uses Area Links to determine reachability, improving performance.

--- Assets/Scripts/ColonyZ/Models/Map/Regions/RegionReachabilityChecker.cs
+++ Assets/Scripts/ColonyZ/Models/Map/Regions/RegionReachabilityChecker.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace ColonyZ.Models.Map.Regions
+﻿namespace ColonyZ.Models.Map.Regions
 {
     public static class RegionReachabilityChecker
     {
-        private static Queue<Region> queue = new Queue<Region>();
-        private static HashSet<Region> visited = new HashSet<Region>();
-
         /// <summary>
-        ///     Determines if a source region can access a target region through its region links.
-        ///     This does not take into consideration traversal permissions for a region.
+        ///     Determines if a source region can access a target region.
         /// </summary>
         /// <param name="_source"></param>
         /// <param name="_target"></param>
@@ -18,30 +12,8 @@ namespace ColonyZ.Models.Map.Regions
         {
             if (_source == null || _target == null) return false;
             if (_source.Area == null || _target.Area == null) return false;
-            if (_source.Area.AreaID == _target.Area.AreaID) return true;
-
-            queue.Clear();
-            visited.Clear();
-            queue.Enqueue(_source);
-            visited.Add(_source);
-
-            while (queue.Count > 0)
-            {
-                var region = queue.Dequeue();
-
-                foreach (var link in region.Links)
-                {
-                    var other = link.GetOther(region);
-                    if (other == null) continue;
-                    if (other.Area.AreaID == _target.Area.AreaID) return true;
-                    if (visited.Contains(other)) continue;
-
-                    queue.Enqueue(other);
-                    visited.Add(other);
-                }
-            }
-
-            return false;
+            
+            return _source.Area.AreaID == _target.Area.AreaID || _source.Area.LinkedAreas.Contains(_target.Area);
         }
     }
 }
