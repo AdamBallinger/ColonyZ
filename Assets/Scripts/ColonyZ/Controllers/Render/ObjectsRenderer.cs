7835045b272dceb8c6d046a43ae01d0d5771f164
Adam Ballinger <crunchienut20@gmail.com>
2021-08-27 19:55:14 +0100

    ObjectRenderer now creates material property blocks for each type of object.

--- Assets/Scripts/ColonyZ/Controllers/Render/ObjectsRenderer.cs
+++ Assets/Scripts/ColonyZ/Controllers/Render/ObjectsRenderer.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using ColonyZ.Models.Map;
 using ColonyZ.Models.Map.Tiles.Objects;
 using ColonyZ.Models.Map.Tiles.Objects.Data;
+using ColonyZ.Models.Sprites;
 using ColonyZ.Models.TimeSystem;
 using ColonyZ.Utils;
 using UnityEngine;
@@ -19,6 +20,8 @@ namespace ColonyZ.Controllers.Render
 
         private Camera cam;
 
+        private Dictionary<TileObjectData, MaterialPropertyBlock> mpbs = new Dictionary<TileObjectData, MaterialPropertyBlock>();
+
         private void Start()
         {
             cam = Camera.main;
@@ -27,10 +30,15 @@ namespace ColonyZ.Controllers.Render
 
             foreach (var data in TileObjectDataCache.ObjectDatas)
             {
+                if (data.SmartObject) continue;
+                
                 var pivot = data.SpriteData.Sprites[0].bounds.center;
                 var size = data.SpriteData.Sprites[0].rect.size / 32;
                 //Debug.Log(data.ObjectName + " : " + data.SpriteData.Sprites[0].rect);
                 meshes.Add(data, MeshUtils.CreateQuad("Quad", (int)size.x, (int)size.y, pivot));
+                var mpb = new MaterialPropertyBlock();
+                mpb.SetTexture("_MainTex", SpriteCache.GetSprite(data.SpriteData.SpriteGroup, 0).texture);
+                mpbs.Add(data, mpb);
             }
         }
 
@@ -43,6 +51,10 @@ namespace ColonyZ.Controllers.Render
         {
             foreach (var obj in World.Instance.Objects)
             {
+                // Dont draw smart objects (Walls, stone, etc.) as they are drawn with the tile shader.
+                if (obj.ObjectData.SmartObject) continue;
+                
+                // Check if the object is in view of the camera (Plus a small border to prevent pop in).
                 var screenPos = cam.WorldToViewportPoint(obj.OriginTile.Position); 
                 var inView = screenPos.z >= -0.1f && 
                              screenPos.x >= -0.1f && 
@@ -51,13 +63,15 @@ namespace ColonyZ.Controllers.Render
                              screenPos.y <= 1.1f;
                 
                 if (!inView) continue;
-                
+
                 Graphics.DrawMesh(meshes[obj.ObjectData],
                     obj.OriginTile.Position,
                     ObjectRotationUtil.GetQuaternion(obj.ObjectRotation),
                     obj.ObjectData.Foliage ? foliageMaterial : basicMaterial,
                     1,
-                    cam);
+                    cam,
+                    0,
+                    mpbs[obj.ObjectData]);
             }
         }
     }
